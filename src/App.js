a2a7beb51e3a07a50f9d42522705fcda70c7721c
Nattanon Yanil <nattanonyanil@gmail.com>
2018-06-16 15:01:43 +0700

    first setup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import AppLogin from './AppLogin';
+import FormA from './FormA';
+import FormB from './FormB';
+import FormC from './FormC';
+
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
+    const childProps = {
+    };
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Switch>
+          <Route path="/login" component={AppLogin} props={childProps}/>
+          <Route path="/formA" component={FormA} props={childProps}/>
+          <Route path="/formB" component={FormB} props={childProps}/>
+          <Route path="/formC" component={FormC} props={childProps}/>
+        </Switch>
       </div>
     );
   }
