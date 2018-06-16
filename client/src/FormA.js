97a22882ebf4de99790b71b538c8d9de5d380bd4
bique14 <peerasorn14@gmail.com>
2018-06-16 15:09:45 +0700

    create client

---
+++ client/src/FormA.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+
+class FormA extends Component {
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+export default FormA;
---
+++ client/src/FormC.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+
+class FormC extends Component {
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+export default FormC;
